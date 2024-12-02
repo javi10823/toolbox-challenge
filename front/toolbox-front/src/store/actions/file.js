86c53f2c6b089651e6681c5502395a0d9bb7b516
javi10823 <javi10823@gmail.com>
2024-12-02 16:52:29 -0300

    removed axios

--- front/toolbox-front/src/store/actions/file.js
+++ front/toolbox-front/src/store/actions/file.js
@@ -1,9 +1,25 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
+
+const fileList = ["file1.csv", "file2.csv"];
 
 const data = [
   {
     file: "file1.csv",
+    lines: [
+      {
+        text: "RgTya",
+        number: 64075909,
+        hex: "70ad29aacf0b690b0467fe2b2767f765",
+      },
+      {
+        text: "RgTya",
+        number: 64075909,
+        hex: "70ad29aacf0b690b0467fe2b2767f765",
+      },
+    ],
+  },
+  {
+    file: "file2.csv",
     lines: [
       {
         text: "RgTya",
@@ -23,9 +39,40 @@ export const getFiles = createAsyncThunk(
   "file/getFiles",
   async (_, { rejectWithValue }) => {
     try {
-      // const response = await axios.get("API_URL");
+      //   const response = await fetch("API_URL");
+      //   const data = await response.json();
 
       return data;
+    } catch (error) {
+      return rejectWithValue(
+        error instanceof Error ? error.message : "Unexpected Error"
+      );
+    }
+  }
+);
+
+export const getFileList = createAsyncThunk(
+  "file/getFileList",
+  async (_, { rejectWithValue }) => {
+    try {
+      //   const response = await fetch("API_URL");
+      //   const data = await response.json();
+      return fileList;
+    } catch (error) {
+      return rejectWithValue(
+        error instanceof Error ? error.message : "Unexpected Error"
+      );
+    }
+  }
+);
+
+export const getFileByName = createAsyncThunk(
+  "file/getFileByName",
+  async (name, { rejectWithValue }) => {
+    try {
+      //   const response = await fetch("API_URL");
+      //   const data = await response.json();
+      return data[0];
     } catch (error) {
       return rejectWithValue(
         error instanceof Error ? error.message : "Unexpected Error"
